01619220383c70769a13f7b4040e52b226bb81db
Aziz Shekerbekov <153740752+aziz200115@users.noreply.github.com>
2024-03-16 15:40:27 -0500

    ASG added

---
+++ asg/data.tf
@@ -0,0 +1,43 @@
+# Pulls VPC related information
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "shekerbekov"
+    workspaces = {
+      name = "vpc"
+    }
+  }
+}
+
+
+
+# DB related information
+data "terraform_remote_state" "vpc" {
+  backend = "remote"
+
+  config = {
+    organization = "shekerbekov"
+    workspaces = {
+      name = "rds"
+    }
+  }
+}
+
+
+# Pulls AMI information
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
---
+++ rds/output.tf
@@ -0,0 +1,4 @@
+output address {
+	description = "Prints out address information for db"
+	value = aws_db_instance.default.address
+}
